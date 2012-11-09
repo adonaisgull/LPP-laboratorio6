9436d72c85296d48cf08a68b7b42e728ba9a7f0d
SUAREZ GONZALEZ <alu4308@cc339.(none)>
2012-11-09 15:49:21 +0000

    Test 5

--- lib/rpsls.rb
+++ lib/rpsls.rb
@@ -7,4 +7,17 @@ class Rpsls
   
   attr_reader :human_choice, :computer_choice, :valid_choices, :beats, :winner
   
+  def initialize()
+    @valid_choices = [:rock, :paper, :scissors, :lizard, :spock]
+  end
+  
+  def get_human(human_choice)
+    
+    human_choice = human_choice.to_sym
+    
+    if @valid_choices.include?(human_choice)
+      @human_choice = human_choice
+    end
+    
+  end
 end
--- test/rpsls_spec.rb
+++ test/rpsls_spec.rb
@@ -24,4 +24,13 @@ describe Rpsls do
     @juego.respond_to?("beats").should == true && @juego.respond_to?("winner").should == true
   end
   
+  it "Se debe invocar al metodo obtener_humano() para recoger la tirada del humano y que esta sea valida (Se pasa opcion valida)" do 
+    @juego.get_human("rock");
+    @juego.valid_choices.include?(@juego.human_choice).should == true
+  end
+  
+  it "Se debe invocar al metodo obtener_humano() para recoger la tirada del humano y que esta sea valida (Se pasa opcion no valida)" do 
+    @juego.get_human("invalid_choice");
+    @juego.valid_choices.include?(@juego.human_choice).should == false
+  end
 end
